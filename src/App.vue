09d03d024f4ec913680c29610c723d39212200e3
alfonso <unclefonso@protonmail.com>
2022-03-04 19:11:11 -0500

    files updated ready to start building board

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainBoard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainBoard from './components/MainBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainBoard
   }
 }
 </script>
---
+++ src/components/MainBoard.vue
@@ -0,0 +1,13 @@
+<template>
+<h2>Tic-Tac-Toe</h2>
+</template>
+
+<script>
+export default {
+  name: 'MainBoard'
+}
+</script>
+
+<style scoped>
+
+</style>
