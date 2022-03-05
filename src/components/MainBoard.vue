12d92e61d2905a1d230a6926f92735089fd260db
alfonso <unclefonso@protonmail.com>
2022-03-04 20:57:04 -0500

    reset button added and game works well

--- src/components/MainBoard.vue
+++ src/components/MainBoard.vue
@@ -1,30 +1,77 @@
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 export default {
   name: 'MainBoard',
   props: {
     msg: String
   },
   setup () {
+    const turn = ref('X') // our initial player and piece
     console.log('setup called')
     const squares = ref([])
     squares.value = Array(9).fill('')
     console.log('squares' + squares.value)
-    return { squares }
+    // check 4 winner evertime board updates
+    const winner = computed(() => checkForWinner(squares.value.flat()))
+    function checkForWinner (value) {
+      console.log('inside checkForWinner value: ' + value)
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // horizontal wins
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], // vertical wins
+        [0, 4, 8],
+        [2, 4, 6] // cross wins
+      ]
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (
+          value[a] &&
+          value[a] === value[b] &&
+          value[a] === value[c]
+        ) {
+          return value[a]
+        }
+      }
+      return null
+    }
+    function takeTurn (n) {
+      // this is where we identify our players
+      console.log('takeTurn value: ' + n)
+      // did this move win? exit this function
+      if (winner.value) {
+        console.log('inside if winner value: ' + winner.value)
+        return
+      }
+      // current player takes a turn...value is assigned board position
+      squares.value[n] = turn.value
+      // flip player
+      turn.value = turn.value === 'X' ? 'O' : 'X'
+      console.log('player: ' + turn.value)
+      // do other things
+    }
+    function reset () {
+      console.log('reset called: ')
+      squares.value = Array(9).fill('')
+    }
+    return { squares, takeTurn, winner, turn, reset }
   }
 }
 // Psuedo Code it
 // What does a tic tac toe game need?
 //
-// - Blank board
-// - Players
-// - Piece names (X / Y )
-// - Take turns
-// - Check 4 Winnner
+// - Blank board - Done
+// - Players - Done
+// - Piece names (X / O ) - Done
+// - Take turns - Done
+// - Check 4 Winnner - Done
 // - Check 4 Tie Game
-// - Play Game
+// - Play Game - Done
 // - resetBoard
 // - ------
+// - History of wins
 // - AI - special feature
 // - Look cool
 
@@ -32,14 +79,22 @@ export default {
 
 <template>
   <h2>{{msg}}</h2>
+  <h2 v-if="winner">WINNER: {{ winner }}</h2>
+  <h2 v-else>Player Move: {{ turn }}</h2>
   <div id="board">
-    <div class="square" v-for="(square, index) in squares" :key="index">
+    <div class="square" v-for="(square, index) in squares" :key="index" @click="takeTurn(index)">
       {{ square }}
     </div>
   </div>
+  <button @click="reset">
+    Reset
+  </button>
 </template>
 
 <style scoped>
+button{
+  margin-top: 40px;
+}
 #board {
   border: 1px solid darkred;
   height: 315px;
